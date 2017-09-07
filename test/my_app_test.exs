2afecff801ebfa7d7c71b91b1cb10842527159d4
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 13:58:38 -0700

    Properties of mocks

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -1,20 +1,22 @@
 defmodule MyAppTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true # mocks share no state so tests can run in parallel.
   require ElixirMock
-  import ElixirMock # because coding past midnight causes bugs.
+  import ElixirMock
 
-  test "should return error string when facebook api returns error" do
-    profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
-    expected_error = "profile not found"
+  test "demonstrating characteristics of mocks" do
+    fake_nil_client = mock_of HTTPoison # one-liner nil-mock definition. Same API as HTTPoison
 
-    with_mock(fake_client) = defmock_of HTTPoison do
-      def get(_), do: {:error, "profile not found"}
+    with_mock(custom_fake_client) = defmock_of HTTPoison do
+      def get(_), do: {:ok, :custom_response}
     end
 
-    {:error, error_details} = MyApp.fetch_facebook_profile(profile_id, fake_client)
-    assert error_details == "failed to fetch profile. REASON: #{expected_error}"
+    assert fake_nil_client.get("https://google.com") == nil
 
-    assert_called fake_client.get("https://api.facebook.com/#{profile_id}")
-    refute_called fake_client.get("https://deprecated-api.facebook.com/#{profile_id}")
+    # mocks are independent
+    assert custom_fake_client.get("https://google.com") == {:ok, :custom_response}
+
+    # the real module HTTPoison is not affected
+    {:ok, real_response} = HTTPoison.get("https://google.com")
+    assert real_response.body == "<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\n<TITLE>301 Moved</TITLE></HEAD><BODY>\n<H1>301 Moved</H1>\nThe document has moved\n<A HREF=\"https://www.google.com/\">here</A>.\r\n</BODY></HTML>\r\n"
   end
 end
