afd900ccb6b1009db74abe4dab579846cb3d9a92
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 13:48:05 -0700

    Lose the first test to reduce noise

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -1,12 +1,6 @@
 defmodule MyAppTest do
   use ExUnit.Case
 
-  test "should return profile returned by facebook api call" do
-    profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
-    {:ok, returned_profile} = MyApp.fetch_facebook_profile(profile_id)
-    assert returned_profile == :dummy_profile
-  end
-
   test "should return error string when facebook api returns error" do
     profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
     expected_error = "profile not found"
