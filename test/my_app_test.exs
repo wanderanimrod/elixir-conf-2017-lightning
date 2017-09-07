eb06f811cd157b9cf1820b26c6ff1d4266039275
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 13:23:27 -0700

    Adds test for error case handling. Fails.

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -3,9 +3,14 @@ defmodule MyAppTest do
 
   test "should return profile returned by facebook api call" do
     profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
-
     {:ok, returned_profile} = MyApp.fetch_facebook_profile(profile_id)
-
     assert returned_profile == :dummy_profile
+  end
+
+  test "should return error string when facebook api returns error" do
+    profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
+    expected_error = "profile not found"
+    {:error, error_details} = MyApp.fetch_facebook_profile(profile_id)
+    assert error_details == "failed to fetch profile. REASON: #{expected_error}"
   end
 end
