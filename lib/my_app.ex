bd6788dae6c39cdde228fa53cfa8b95ab57a84eb
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 13:45:15 -0700

    Injecting http_client as dependency in error case test. Defaulting to config-set value in other cases

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -2,11 +2,11 @@ defmodule MyApp do
 
   @http_client Application.get_env(:my_app, :http_client, HTTPoison)
 
-  def fetch_facebook_profile(profile_id) do
-    response = @http_client.get("https://api.facebook.com/#{profile_id}")
+  def fetch_facebook_profile(profile_id, http_client \\ @http_client) do
+    response = http_client.get("https://api.facebook.com/#{profile_id}")
     case response do
       {:ok, json_profile} -> {:ok, json_profile}
-      {:error, reason} -> {:error, "failed to fetch profile. REASON: #{inspect reason}"}
+      {:error, reason} -> {:error, "failed to fetch profile. REASON: #{reason}"}
     end
   end
 end
--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -10,7 +10,12 @@ defmodule MyAppTest do
   test "should return error string when facebook api returns error" do
     profile_id = "B16C4D31-77E4-4AB7-9665-E0EEFC6233C9"
     expected_error = "profile not found"
-    {:error, error_details} = MyApp.fetch_facebook_profile(profile_id)
+
+    defmodule MyAppTest.FakeClient do
+      def get(_), do: {:error, "profile not found"}
+    end
+
+    {:error, error_details} = MyApp.fetch_facebook_profile(profile_id, MyAppTest.FakeClient)
     assert error_details == "failed to fetch profile. REASON: #{expected_error}"
   end
 end
