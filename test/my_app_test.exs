3f12002f69774c7455471a34e6fcfe970500b559
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 16:25:26 -0700

    fixes order of mocks for flow

--- test/my_app_test.exs
+++ test/my_app_test.exs
@@ -4,18 +4,18 @@ defmodule MyAppTest do
   import ElixirMock
 
   test "demonstrating characteristics of mocks" do
-    fake_nil_client = mock_of HTTPoison # one-liner nil-mock definition. Same API as HTTPoison
-
-    with_mock(custom_fake_client) = defmock_of HTTPoison do
+    with_mock(custom_client) = defmock_of HTTPoison do
       def get(_), do: {:ok, :custom_response}
     end
 
-    assert fake_nil_client.get("https://google.com") == nil
+    # one-liner nil-mock definition. Same API as HTTPoison
+    nil_client = mock_of HTTPoison
+    assert nil_client.get("https://google.com") == nil
 
     # mocks are independent
-    assert custom_fake_client.get("https://google.com") == {:ok, :custom_response}
+    assert custom_client.get("https://google.com") == {:ok, :custom_response}
 
-    # the real module HTTPoison is not affected
+    # the real module :HTTPoison is not affected
     {:ok, real_response} = HTTPoison.get("https://google.com")
     assert real_response.body == "<HTML><HEAD><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\">\n<TITLE>301 Moved</TITLE></HEAD><BODY>\n<H1>301 Moved</H1>\nThe document has moved\n<A HREF=\"https://www.google.com/\">here</A>.\r\n</BODY></HTML>\r\n"
   end
