d72d1d80ce08537bdc1c5dc2231051e21df1bce9
Nimrod Wandera <wanderanimrod@gmail.com>
2017-09-07 13:11:48 -0700

    Back to failing test

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -1,9 +1,6 @@
 defmodule MyApp do
-
-  @http_client Application.get_env(:my_app, :http_client, HTTPoison)
-
   def fetch_facebook_profile(profile_id) do
-    response = @http_client.get("https://api.facebook.com/#{profile_id}")
+    response = HTTPoison.get("https://api.facebook.com/#{profile_id}")
     case response do
       {:ok, json_profile} -> {:ok, json_profile}
       {:error, reason} -> {:error, "failed to fetch profile. REASON: #{inspect reason}"}
